31e8fe3b0ac7ec3f030ab1f7b6de14deb4f6d85e
emilrp <32249795+emilrp@users.noreply.github.com>
2022-01-31 16:16:23 +0800

    push emil

---
+++ berkas/foto.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+include "../koneksi.php";
+$id = $_SESSION['id_siswa'];
+$siswa = mysqli_fetch_array(mysqli_query($koneksi,"SELECT * FROM siswa WHERE id ='$id'"));
+?>
+<embed src="../file/siswa/foto/<?= $siswa['foto']?>" width="100%" height="100%" type="application/pdf">
---
+++ berkas_admin/bpjs.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+include "../koneksi.php";
+$id = $_GET['id'];
+$siswa = mysqli_fetch_array(mysqli_query($koneksi,"SELECT * FROM siswa WHERE id ='$id'"));
+?>
+<embed src="../file/siswa/bpjs/<?= $siswa['bpjs']?>" width="100%" height="100%" type="application/pdf">
